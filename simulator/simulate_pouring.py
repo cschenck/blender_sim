2ecb4d1bc573d884b568a77f51fc58bdd01480fe
Connor <connor.schenck@gmail.com>
2016-10-18 13:31:46 -0700

    Added comments to the arguments for simulate_pouring.py

--- simulator/simulate_pouring.py
+++ simulator/simulate_pouring.py
@@ -20,51 +20,86 @@ else:
     sys.argv = sys.argv[1:]
 
 parser = argparse.ArgumentParser(sys.argv)
-# Bake arguments.
-parser.add_argument('--init_value', action="store", dest="init_value", type=float, default=0.5)
-parser.add_argument('--sim_time', action="store", dest="sim_time", type=float, default=15.0)
-parser.add_argument('--pour_ang_vel', action="store", dest="pour_ang_vel", type=float, default=9.0)
-parser.add_argument('--return_ang_vel', action="store", dest="return_ang_vel", type=float, default=-9.0)
-parser.add_argument('--pour_end_time', action="store", dest="pour_end_time", type=float, default=10.0)
-parser.add_argument('--return_start_time', action="store", dest="return_start_time", type=float, default=15.0)
-parser.add_argument('--pouring_object', action="store", dest="pouring_object", type=str, default="cup")
-parser.add_argument('--bowl_object', action="store", dest="bowl_object", type=str, default="bowl")
-parser.add_argument('--alternate_bowl', action="store", dest="alternate_bowl", type=str, default=None)
 
-# Render arguments
-parser.add_argument('--camera_loc', action="store", dest="camera_loc", type=lambda x : cardinal_angles[x], default='N')
-parser.add_argument('--background_rotation', action="store", dest="background_rotation", type=lambda x : cardinal_angles[x], default='N')
-parser.add_argument('--video_start_ratio', action="store", dest="video_start_ratio", type=float, default=-1.0) #DEPRICATED
-parser.add_argument('--use_video_plane', action="store_true", dest="use_video_plane", default=True)
-parser.add_argument('--composite_video_plane', action="store_true", dest="composite_video_plane", default=False)
-parser.add_argument('--no_composite_video_plane', action="store_false", dest="composite_video_plane")
-parser.add_argument('--render_pouring_object', action="store", dest="render_pouring_object", type=str, default=None)
-parser.add_argument('--render_glass_cup', action="store_true", dest="render_glass_cup", default=False)
-parser.add_argument('--render_cup', action="store_true", dest="render_cup", default=True)
-parser.add_argument('--no_render_cup', action="store_false", dest="render_cup")
-parser.add_argument('--camera_pitch', action="store", dest="camera_pitch", type=lambda x : {'high':math.pi/4.0,'low':0.0}[x], default='high')
-parser.add_argument('--camera_dis', action="store", dest="camera_dis", type=lambda x : {'close':8.0,'normal':10.0,'far':12.0}[x], default='normal')
-parser.add_argument('--bowl_mat_preset', action="store", dest="bowl_mat_preset", type=int, default=None)
-parser.add_argument('--cup_mat_preset', action="store", dest="cup_mat_preset", type=int, default=None)
-parser.add_argument('--water_alpha', action="store", dest="water_alpha", type=float, default=0.0)
-parser.add_argument('--water_reflect', action="store", dest="water_reflect", type=float, default=0.2)
-parser.add_argument('--water_ior', action="store", dest="water_ior", type=float, default=1.33)
-parser.add_argument('--render_water', action="store_true", dest="render_water")
-parser.add_argument('--no_render_water', action="store_false", dest="render_water")
-parser.add_argument('--bool_render_water', action="store", type=str, dest="bool_render_water")
+# General arguments used for either rendering or simulation.
+parser.add_argument('--bake', action="store_true", dest="bake", default=False,
+    help='Run in simulation mode (i.e., "bake" the simulation).')
+parser.add_argument('--render', action="store_true", dest="render", default=False,
+    help='Run in render mode.')
+parser.add_argument('--track_cup_lip', action="store_true", dest="track_cup_lip", default=False,
+    help='After rendering, generate a text file with the x,y pixel coordinates of the lip of the pouring object for each frame on each line.')
+parser.add_argument('--dry_run', action="store_true", dest="dry_run", default=False,
+    help='Don\'t actually execute any simulation or rendering. Just ensure that everything will run properly. Useful for verifying everything is setup correctly before attempting to run.')
+parser.add_argument('--tempdir', action="store", dest="tempdir", type=str, default=None,
+    help='The simulation and rendering processes use a significant amount of temporary files. By default, they use the default temporary file storage. Set this variable to have them use a user-specified directory for temporary files.')
 
-parser.add_argument('--bake', action="store_true", dest="bake", default=False)
-parser.add_argument('--render', action="store_true", dest="render", default=False)
-parser.add_argument('--track_cup_lip', action="store_true", dest="track_cup_lip", default=False)
-# parser.add_argument('--load', action="store_true", dest="load", default=False)
-# parser.add_argument('--render_path', action="store", dest="render_path", type=str, default=None)
-# parser.add_argument('--blend_file', action="store", dest="blend_file", type=str, default=None)
-# parser.add_argument('--bake_path', action="store", dest="bake_path", type=str, default=None)
-parser.add_argument('--data_prefix', action="store", dest="data_prefix", type=str, default="data")
-parser.add_argument('--gt_prefix', action="store", dest="gt_prefix", type=str, default="ground_truth")
+# Simulation arguments. Only needed when simulating the fluid trajectory.
+parser.add_argument('--init_value', action="store", dest="init_value", type=float, default=0.5,
+    help='The initial ratio to fill the cup [0-1].')
+parser.add_argument('--sim_time', action="store", dest="sim_time", type=float, default=15.0,
+    help='The number of seconds to run the simulation for.')
+parser.add_argument('--pour_end_time', action="store", dest="pour_end_time", type=float, default=10.0,
+    help='The time at which to end the initial tild downward of the cup (which starts immediately).')
+parser.add_argument('--pour_ang_vel', action="store", dest="pour_ang_vel", type=float, default=9.0,
+    help='The angular velocity of the cup during the initial tilt down of the cup.')
+parser.add_argument('--return_start_time', action="store", dest="return_start_time", type=float, default=15.0,
+    help='The time to start tilting the cup back upright, which continues until the sim times out.')
+parser.add_argument('--return_ang_vel', action="store", dest="return_ang_vel", type=float, default=-9.0,
+    help='The angular velocity while tilting the cup back upright.')
+parser.add_argument('--pouring_object', action="store", dest="pouring_object", type=str, default="cup",
+    help='The source container to pour from. May be cup, bottle, or mug.')
+parser.add_argument('--bowl_object', action="store", dest="bowl_object", type=str, default="bowl",
+    help='The target container to pour into. May be bowl, dish, or fruit.')
+parser.add_argument('--alternate_bowl', action="store", dest="alternate_bowl", type=str, default=None,
+    help='DEPRICATED: Switches out the target container with a cup. Same values as for pouring_object.')
 
-parser.add_argument('--dry_run', action="store_true", dest="dry_run", default=False)
-parser.add_argument('--tempdir', action="store", dest="tempdir", type=str, default=None)
+# Render arguments. Only needed when rendering a simulation.
+parser.add_argument('--camera_loc', action="store", dest="camera_loc", type=lambda x : cardinal_angles[x], default='N',
+    help='The azimuth of the camera around the table. Can be N, NW, W, SW, S, SE, E, NE.')
+parser.add_argument('--background_rotation', action="store", dest="background_rotation", type=lambda x : cardinal_angles[x], default='N',
+    help='The rotation of the background sphere. Can be N, NW, W, SW, S, SE, E, NE. If using a background video, which video to use is selected by determing what part of the background is being looked at by the camera by coming camera_loc with this variable.')
+parser.add_argument('--video_start_ratio', action="store", dest="video_start_ratio", type=float, default=-1.0,
+    help='DEPRICATED: don\'t use') #DEPRICATED
+parser.add_argument('--use_video_plane', action="store_true", dest="use_video_plane", default=True,
+    help='Add this argument to insert a background video behind the table.')
+parser.add_argument('--composite_video_plane', action="store_true", dest="composite_video_plane", default=False,
+    help='If set, instead of adding the background video to the render, the video is added after the render is finished. This was added to see if adding the video after the fact would be faster, but it is not and it looks worse. This option is not recommended.')
+parser.add_argument('--no_composite_video_plane', action="store_false", dest="composite_video_plane",
+    help='Disables --composite_video_plane.')
+parser.add_argument('--render_pouring_object', action="store", dest="render_pouring_object", type=str, default=None,
+    help='This can be either True or False (passed as --render_pouring_object True). If False, the pouring object is not rendered.')
+parser.add_argument('--render_glass_cup', action="store_true", dest="render_glass_cup", default=False,
+    help='This can be either True or False (passed as --render_glass_cup True). If true, the pouring object is rendered as glass.')
+parser.add_argument('--render_cup', action="store_true", dest="render_cup", default=True,
+    help='If set, the pouring object is rendered.')
+parser.add_argument('--no_render_cup', action="store_false", dest="render_cup",
+    help='If set, the pouring object is not rendered.')
+parser.add_argument('--camera_pitch', action="store", dest="camera_pitch", type=lambda x : {'high':math.pi/4.0,'low':0.0}[x], default='high',
+    help='The pitch of the camera. "high" is looking down into the target container at a 45 degree angle. "low" is looking directly at the side of the target container.')
+parser.add_argument('--camera_dis', action="store", dest="camera_dis", type=lambda x : {'close':8.0,'normal':10.0,'far':12.0}[x], default='normal',
+    help='The camera distance from the scene. Can be close, normal, or far.')
+parser.add_argument('--bowl_mat_preset', action="store", dest="bowl_mat_preset", type=int, default=None,
+    help='The target container texture preset to use. Can be 0 to 6 inclusive.')
+parser.add_argument('--cup_mat_preset', action="store", dest="cup_mat_preset", type=int, default=None,
+    help='The pouring object texture preset to use. Can be 0 to 6 inclusive.')
+parser.add_argument('--water_alpha', action="store", dest="water_alpha", type=float, default=0.0,
+    help='The transparency of the liquid, where 0 is fully transparent and 1 is fully opaque.')
+parser.add_argument('--water_reflect', action="store", dest="water_reflect", type=float, default=0.2,
+    help='The reflectance of the liquid. See Blender\'s liquid property settings for details.')
+parser.add_argument('--water_ior', action="store", dest="water_ior", type=float, default=1.33,
+    help='The index of refraction for the liquid.')
+parser.add_argument('--render_water', action="store_true", dest="render_water",
+    help='DEPRICATED: use --bool_render_water instead.')
+parser.add_argument('--no_render_water', action="store_false", dest="render_water",
+    help='DEPRICATED: use --bool_render_water instead.')
+parser.add_argument('--bool_render_water', action="store", type=str, dest="bool_render_water",
+    help='This can be either True or False (passed as --bool_render_water True). If false, the liquid is not shown in the render.')
+parser.add_argument('--data_prefix', action="store", dest="data_prefix", type=str, default="data",
+    help='The prefix for the rendered images. The files will be of the form DATA_PREFIX\%04d.png where \%04d is the frame number.')
+parser.add_argument('--gt_prefix', action="store", dest="gt_prefix", type=str, default="ground_truth",
+    help='The prefix for the pixel labels of the rendered images. The files will be of the form GT_PREFIX\%04d.png where \%04d is the frame number.')
+
+
 args = parser.parse_args()
 
 if args.tempdir:
